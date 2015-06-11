4be00d355cb6fa5b1741eb11370d8feb22d97858
James Moriarty <james.moriarty@rea-group.com>
2015-06-12 07:45:54 +1000

    bugfix: https.

--- lib/poto/helpers/url_helper.rb
+++ lib/poto/helpers/url_helper.rb
@@ -3,11 +3,11 @@ module Poto
     def url_for(opts, path, query = {})
       request = Grape::Request.new(opts[:env])
 
-      URI::HTTP.build(
-        host:  request.host,
-        port:  request.port,
-        path:  File.join(opts[:env]["SCRIPT_NAME"], path),
-        query: query.to_param
+      URI::Generic.build(
+        host:   request.host,
+        port:   request.port,
+        path:   File.join(opts[:env]["SCRIPT_NAME"], path),
+        query:  query.to_param
       ).to_s.gsub(/\?$/, "")
     end
 
