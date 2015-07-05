e0b164f14d808fd1c35aeba0fbe8161e8a998a7d
James Moriarty <james.moriarty@rea-group.com>
2015-07-05 19:18:11 +1000

    refinement: extact #cache_file.

--- lib/poto/file_cache.rb
+++ lib/poto/file_cache.rb
@@ -9,11 +9,17 @@ module Poto
     end
 
     def cache(key, &set)
-      File.join(path, Digest::SHA256.hexdigest(key)).tap do |cache_path|
+      File.join(path, cache_file(key)).tap do |cache_path|
         unless File.exists?(cache_path)
           FileUtils.cp(set.call, cache_path)
         end
       end
+    end
+
+    private
+
+    def cache_file(key)
+      Digest::SHA256.hexdigest(key)
     end
   end
 end
