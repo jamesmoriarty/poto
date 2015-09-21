a563eb4668d81572590706d03ad614f340634991
James Moriarty <jamespaulmoriarty@gmail.com>
2015-09-21 20:36:25 +1000

    bugfix: tests.

--- spec/support/customer_matchers.rb
+++ spec/support/customer_matchers.rb
@@ -2,6 +2,6 @@ RSpec::Matchers.define :be_json_of_files do |expected|
   match do |actual|
     expect(actual[:_embedded][:files].size).to eq expected.size
     expect(actual[:_links][:self]).to eq(href: "//example.org:80/files?page=")
-    expect(actual[:_links][:next]).to eq(href: "//example.org:80/files?page=example.png")
+    expect(actual[:_links][:next]).to eq(href: "//example.org:80/files?page=example.png&per_page=25")
   end
 end
