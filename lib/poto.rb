743f34091f603cd5fcf30f105a3beb38d374899f
James Moriarty <jamespaulmoriarty@gmail.com>
2016-02-06 22:50:15 +1100

    update: gems.

--- lib/poto.rb
+++ lib/poto.rb
@@ -1,3 +1,6 @@
+require 'active_support'
+require 'active_support/core_ext/object/to_param'
+require 'active_support/core_ext/object/try'
 require "poto/version"
 require "poto/api"
 require "poto/image_proxy"
