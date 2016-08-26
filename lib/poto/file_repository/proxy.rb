a44e479b974ff7486fb2b5201550a742ab6155dc
James Moriarty <james.moriarty@rea-group.com>
2016-08-26 23:57:52 +1000

    refinement: proxy.

--- lib/poto/file_repository/proxy.rb
+++ lib/poto/file_repository/proxy.rb
@@ -1,35 +1,35 @@
 module Poto
   module FileRepository
     class Proxy
-      attr_writer :page, :per_page, :prefix
+      attr_reader :backend
 
       def initialize(backend)
         @backend = backend
       end
 
       def url(id)
-        @backend.url(id)
+        backend.url(id)
       end
 
       def all
-        @backend.all(prefix: @prefix, page: @page, per_page: @per_page)
+        backend.all(prefix: @prefix, page: @page, per_page: @per_page)
       end
 
       def page(page)
         tap do |proxy|
-          proxy.page = page
+          @page = page
         end
       end
 
       def per_page(per_page)
         tap do |proxy|
-          proxy.per_page = per_page
+          @per_page = per_page
         end
       end
 
       def prefix(prefix)
         tap do |proxy|
-          proxy.prefix = prefix
+          @prefix = prefix
         end
       end
     end
