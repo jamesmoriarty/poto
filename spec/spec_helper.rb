f7ddf25201b224b9dfa894f1f9195f20f65b93bf
James Moriarty <james.moriarty@rea-group.com>
2016-11-24 18:54:40 +1100

    refinement: code climate.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,3 @@
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
-
 Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each { |file| require file }
 
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
